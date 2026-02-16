f4373432f65291f84a17c04a06d27a5c78779fea
boyswan <jackwilliamboyce@gmail.com>
2026-02-16 12:32:09 +0000

    Integrate conform.nvim for code formatting
    Added configuration for conform.nvim to format JavaScript and TypeScript files.

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -263,7 +263,27 @@ return {
   {
     "nvimtools/none-ls.nvim",
   },
-
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      vim.keymap.set("n", "<leader>f", function()
+        require("conform").format({
+          async = true,
+          lsp_format = "fallback",
+        })
+      end, { desc = "Format buffer" })
+      require("conform").setup({
+        formatters_by_ft = {
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        },
+      })
+    end,
+  },
   {
     "j-hui/fidget.nvim",
     config = function()
