dd31138c28422149068bb2bcc55426baabe26dea
boyswan <jackwilliamboyce@gmail.com>
2026-05-20 21:05:46 +0100

    fix: more zsh

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -1,5 +1,33 @@
 cd_fzf() {
-  cd $HOME && cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)" && echo "$PWD" && tree -L 2
+  local dir
+
+  cd "$HOME" || return
+  dir=$(fd -t d . Projects | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)
+  if [ -n "$dir" ]; then
+    cd "$dir" || return
+    echo "$PWD"
+    tree -L 2
+  fi
+}
+
+file_fzf() {
+  local file file_path dir filename
+
+  cd "$HOME" || return
+  file=$(fd -t f . Projects | fzf --preview="bat --color=always --style=numbers --line-range=:500 {}" --bind="space:toggle-preview" --preview-window=:hidden)
+  if [ -n "$file" ]; then
+    file_path="${file:A}"
+    dir="${file_path:h}"
+    filename="${file_path:t}"
+    cd "$dir" || return
+    echo "Changed to: $PWD"
+    echo "Selected: $filename"
+    "${EDITOR:-nvim}" "$filename"
+  fi
+}
+
+open_fzf() {
+  file_fzf "$@"
 }
 
 history_fzf() {
--- dot_config/zsh/keybindings.zsh
+++ dot_config/zsh/keybindings.zsh
@@ -1,5 +1,11 @@
 bindkey -s '^F' 'cd_fzf\n'
-bindkey -s '^V' 'nvim\n'
-bindkey -s '^O' 'open_fzf\n'
+bindkey -s '^E' 'file_fzf\n'
 bindkey -s '^h' 'history_fzf\n'
+bindkey '^[^?' backward-kill-word
 bindkey '^l' expand-or-complete
+
+zmodload -i zsh/complist
+bindkey -M menuselect '^K' up-line-or-history
+bindkey -M menuselect '^J' down-line-or-history
+bindkey -M menuselect '^U' backward-word
+bindkey -M menuselect '^D' forward-word
